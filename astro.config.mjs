6851e37e12b0ba7fd869bfb3bca2f1fbdf7ac4d7
montoyitadevelp <simon1018226487@gmail.com>
2024-01-14 16:10:43 -0500

    feat: config + vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,8 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from "@astrojs/tailwind";
 
+import vercelStatic from '@astrojs/vercel/static';
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
@@ -12,5 +14,7 @@ export default defineConfig({
   routing: {
     prefixDefaultLocale: false
   },
+  output: 'static',
+  adapter: vercelStatic(),
 
 });
